0f2641908529259016b568c5eeac39e42ea68c9e
arvidn <arvid@cs.umu.se>
2017-01-05 01:08:20 -0500

    fix client_test

--- examples/session_view.cpp
+++ examples/session_view.cpp
@@ -95,8 +95,8 @@ void session_view::render()
 	print(str);
 
 	std::snprintf(str, sizeof(str), "%s%swaste: %s   up: %s (%s) "
-		"disk queue: %s | %s cache w: %3d%% r: %3d%% "
-		"size: w: %s r: %s total: %s       %s\x1b[K"
+		"disk queue: %s | %s cache w: %3d%% "
+		"total: %s       %s\x1b[K"
 #ifdef _WIN32
 		, esc("40")
 #else
@@ -110,10 +110,6 @@ void session_view::render()
 		, color(to_string(int(m_cnt[0][m_queued_writes_idx]), 3), col_green).c_str()
 		, int((m_cnt[0][m_blocks_written_idx] - m_cnt[0][m_write_ops_idx]) * 100
 			/ (std::max)(std::int64_t(1), m_cnt[0][m_blocks_written_idx]))
-		, int(m_cnt[0][m_cache_hit_idx] * 100
-			/ (std::max)(std::int64_t(1), m_cnt[0][m_num_blocks_read_idx]))
-		, add_suffix(m_cnt[0][m_writes_cache_idx] * 16 * 1024).c_str()
-		, add_suffix(m_cnt[0][m_reads_cache_idx] * 16 * 1024).c_str()
 		, add_suffix(m_cnt[0][m_blocks_in_use_idx] * 16 * 1024).c_str()
 		, esc("0"));
 	set_cursor_pos(0, y++);
@@ -135,47 +131,7 @@ void session_view::render()
 
 	set_cursor_pos(0, y++);
 	print(str);
-
-	std::snprintf(str, sizeof(str), "|  cache  - total: %4d read: %4d write: %4d pinned: %4d write-queue: %4d"
-		, cs.read_cache_size + cs.write_cache_size, cs.read_cache_size
-		, cs.write_cache_size, cs.pinned_blocks
-		, int(m_cnt[0][m_queued_bytes_idx] / 0x4000));
-	set_cursor_pos(0, y++);
-	print(str);
 */
-	int mru_size = int(m_cnt[0][m_mru_size_idx] + m_cnt[0][m_mru_ghost_idx]);
-	int mfu_size = int(m_cnt[0][m_mfu_size_idx] + m_cnt[0][m_mfu_ghost_idx]);
-	int arc_size = mru_size + mfu_size;
-
-	char mru_caption[100];
-	std::snprintf(mru_caption, sizeof(mru_caption), "MRU: %d (%d)"
-		, int(m_cnt[0][m_mru_size_idx]), int(m_cnt[0][m_mru_ghost_idx]));
-	char mfu_caption[100];
-	std::snprintf(mfu_caption, sizeof(mfu_caption), "MFU: %d (%d)"
-		, int(m_cnt[0][m_mfu_size_idx]), int(m_cnt[0][m_mfu_ghost_idx]));
-
-	pos = std::snprintf(str, sizeof(str), "cache: ");
-	if (arc_size > 0)
-	{
-		if (mru_size > 0)
-		{
-			pos += std::snprintf(str + pos, sizeof(str) - pos, "%s"
-				, progress_bar(int(m_cnt[0][m_mru_ghost_idx] * 1000 / mru_size)
-					, mru_size * (m_width-8) / arc_size, col_yellow, '-', '#'
-					, mru_caption, progress_invert).c_str());
-		}
-		pos += std::snprintf(str + pos, sizeof(str) - pos, "|");
-		if (mfu_size)
-		{
-			pos += std::snprintf(str + pos, sizeof(str) - pos, "%s"
-				, progress_bar(int(m_cnt[0][m_mfu_size_idx] * 1000 / mfu_size)
-					, mfu_size * (m_width-8) / arc_size, col_green, '#', '-'
-					, mfu_caption).c_str());
-		}
-	}
-	pos += std::snprintf(str + pos, sizeof(str) - pos, "\x1b[K");
-	set_cursor_pos(0, y++);
-	print(str);
 
 	if (m_print_utp_stats)
 	{
